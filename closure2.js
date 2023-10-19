9c00c1d2c26766669e298538a698248d743f4539
CP-Rahul <rahulcp1401@gmail.com>
2023-10-19 18:41:30 +0530

    Added a complex example of closure

---
+++ closure2.js
@@ -0,0 +1,21 @@
+function process() {
+    let i = 0;
+    function innerprocess1() {
+        i += 1;
+        return i;
+    }
+    function innerprocess2() {
+        i += 2;
+        return i;
+    }
+    return {innerprocess1, innerprocess2};
+}
+
+let result = process();
+let obj = {func1: result.innerprocess1,
+          func2: result.innerprocess2}
+console.log(result.innerprocess1());
+console.log(result.innerprocess2());
+console.log(result.innerprocess2());
+console.log(result.innerprocess1());
+
